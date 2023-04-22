b3e57df60904e538c3ba2a1c6ee2768dc3aff8a2
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-04-22 21:08:29 +0100

    final project

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import styled from "@emotion/styled";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
 
+  align-items: center;
+  width: 48rem;
+align-items: center;
+justify-content: center;
+text-align: center;
 `;
 
 const Button = styled.button`
